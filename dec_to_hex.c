8fce8b083f10ff806a450dde71e950e2fd4a61e4
Russell Langham Jr <rjlangham2004@gmail.com>
2026-02-06 15:08:04 -0500

    Implement decimal-to-hex pipe conversion utility

--- dec_to_hex.c
+++ dec_to_hex.c
@@ -3,29 +3,25 @@
 #include <string.h>
 
 static void trim_newline(char *s) {
-    if (!s) return;
     size_t n = strlen(s);
-    if (n && s[n - 1] == '\n') s[n - 1] = '\0';
+    while (n > 0 && (s[n - 1] == '\n' || s[n - 1] == '\r')) {
+        s[n - 1] = '\0';
+        n--;
+    }
 }
 
-void dec_to_hex(char* decimal_str, char* hex_str) {
-    if (!decimal_str || !hex_str) return;
+int main(void) {
+    char decimal_str[256];
+
+    if (!fgets(decimal_str, sizeof(decimal_str), stdin)) {
+        return 0;
+    }
 
     trim_newline(decimal_str);
 
-    unsigned long value = strtoul(decimal_str, NULL, 10);
+    unsigned long long value = strtoull(decimal_str, NULL, 10);
 
-    // uppercase hex + newline (nice for pipes)
-    snprintf(hex_str, 8, "%lX\n", value);
-}
+    printf("%llX", value);
 
-int main() {
-    char decimal_str[8] = {0};
-    fgets(decimal_str, sizeof(decimal_str), stdin);
-
-    char hex_str[8] = {0};
-    dec_to_hex(decimal_str, hex_str);
-
-    printf("%s", hex_str);
     return 0;
 }
